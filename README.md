a443e9b7e559ecc66717b78d75036d41493f60f3
arongk <gerson.aron@yahoo.com>
2016-06-13 22:15:42 +0530

    minor edits

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 A sales sample application using HTML5, jQuery, CSS, AngularJs.
 
-
+demo video in media/demo.webm
---
+++ demo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html ng-app="account">
+    <head>
+        <title>account</title>
+        <script src="js/jquery.js" type="text/javascript"></script>
+        <script src="js/angular.min.js" type="text/javascript"></script>
+        <script src="js/angular-resource.js" type="text/javascript"></script>
+        <script src="js/angularjs_route.js" type="text/javascript"></script>
+        <script src="js/ui-router.js" type="text/javascript"></script>
+        <script src="js/fusioncharts.js" type="text/javascript"></script>
+        <script src="js/fusioncharts.charts.js" type="text/javascript"></script>
+        <script src="js/account.js" type="text/javascript"></script>
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body ng-controller="mainController">
+        <video width="100%" controls>
+            <source src="media/demo.webm" type="video/webm">
+            Your browser does not support the video tag.
+        </video>
+    </body>
+</html>
